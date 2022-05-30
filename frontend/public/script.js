8b26765f399bbd67e7db6edee6db146ada51bfd6
NyergesC <cintia.nyerges@gmail.com>
2022-05-30 22:45:14 +0200

    email pattern corrected

--- frontend/public/script.js
+++ frontend/public/script.js
@@ -102,7 +102,7 @@ const formComponent = () => {
                     </div>
                     <div class="input-field">
                         <label for="email">E-mail:</label>                       
-                        <input id="email" type="text" name="email" pattern=".+@globex\.com" size="30" required value="default@example.com">                                                
+                        <input id="email" type="text" name="email" pattern="[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,4}$" required value="default@example.com">                                                
                     </div>
                     <div class="input-field">
                     <label for="title">Photo's title:</label>                       
